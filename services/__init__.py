0eb7cd0a965e402997564432680d0d5588e85ba7
KKNOBUG <807440781@qq.com>
2026-08-09 15:33:47 +0800

    ...

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from .ctx import CTX_USER_ID, CTX_USERNAME, get_current_username
 from .dependency import AuthControl, DependAuth, DependOptionalAuth, DependPermission
 from .password import verify_password, get_password_hash, generate_password, create_access_token
